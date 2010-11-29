8a3bc16ae9d57fab3cb135476941f3d03a0e1838
Alex Heinz <xale@acm.jhu.edu>
2010-11-28 20:06:11 -0500

    Added some internal signals to, and wrote some boilerplate for the phase-locked-loop entity.

--- PhaseLockedLoop250kHz.vhd
+++ PhaseLockedLoop250kHz.vhd
@@ -41,6 +41,60 @@ end PhaseLockedLoop250kHz;
 
 architecture Behavioral of PhaseLockedLoop250kHz is
 	
+	-- Internal signals
+	-- Clock-synched input signal
+	signal lockSignal	: std_logic;
+	
+	-- Phase-accumulator value
+	signal phaseAccumulator		: signed(31 downto 0);
+	signal nextAccumulatorValue	: signed(31 downto 0);
+	
+	-- Alias for MSB of phase accumulator
+	alias accumulatorMSB	: std_logic is phaseAccumulator(31);
+	
 begin
+	
+	-- Input-signal synchronizer process
+	process (clk100MHz, reset)
+	begin
+		-- Clear latched signal value on reset
+		if (reset = AH_ON) then
+			lockSignal <= '0';
+		-- Latch new signal value from input signal on clock edges
+		elsif rising_edge(clk100MHz) then
+			lockSignal <= lockSignal_external;
+		end if;
+	end process;
+	
+	-- Accumulator process
+	process (clk100MHz, reset)
+	begin
+		-- Clear accumulator on reset
+		if (reset = AH_ON) then
+			phaseAccumulator <= (others => '0');
+		-- Latch next accumulator value on clock edges
+		elsif rising_edge(clk100MHz) then
+			phaseAccumulator <= nextAccumulatorValue;
+		end if;
+	end process;
+	
+	-- FIXME: WRITEME: next-accumulator-value logic
+	
+	-- Frequency/phase locking adjustments process
+	process (lockSignal, reset)
+	begin
+		if (reset = AH_ON) then
+			-- FIXME: WRITEME
+		elsif falling_edge(lockSignal) then
+			-- FIXME: WRITEME
+		end if;
+	end process;
+	
+	-- FIXME: WRITEME: next-values logic
+	
+	-- Output signal:
+	-- If the lock is disabled, pass the input through; (after synching)
+	-- otherwise, use the MSB of the accumulator
+	signalOut <= accumulatorMSB when (lockEnable = AH_ON) else lockSignal;
 	
 end Behavioral;
